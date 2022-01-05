14f42e5351e183a612e6c115c6f4ac88b0ff5563
Alexandru Frangulea <a.frangulea@gmail.com>
2022-01-05 18:28:08 +0200

    Added addFish method and form, as well as state for fish instances

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,25 @@ import Header from "./Header.js";
 import Order from "./Order.js";
 import Inventory from "./Inventory.js";
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    console.log("adding fish");
+    //1. Take copy of existing state
+    const fishes = { ...this.state.fishes };
+
+    //2. Add the new fish to fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+
+    //3. Set the new fishes object to state
+    this.setState({
+      fishes: fishes,
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,8 +30,7 @@ class App extends React.Component {
         </div>
 
         <Order />
-
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,16 +2,32 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  goToStore(event) {
+  //   constructor() {
+  //     super();
+  //     this.goToStore = this.goToStore.bind(this);
+  //     console.log("before component creation");
+  //   }
+
+  //create a refference
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    //1. Stop form from submitting
     event.preventDefault();
-    console.log("store");
-  }
+
+    //2. Get text from input (without touching the DOM)
+    const storeName = this.myInput.current.value;
+
+    //3. Change URL with store name
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please select store</h2>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
