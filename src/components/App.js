d0117b1a1ca6ecaa9ef31b27dcdd51cd26894b1f
Alexandru Frangulea <a.frangulea@gmail.com>
2022-01-11 12:18:53 +0200

    Added components and created state for adding fish

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header.js";
 import Order from "./Order.js";
 import Inventory from "./Inventory.js";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 class App extends React.Component {
   state = {
     fishes: {},
@@ -9,7 +11,7 @@ class App extends React.Component {
   };
 
   addFish = (fish) => {
-    console.log("adding fish");
+    console.log(fish);
     //1. Take copy of existing state
     const fishes = { ...this.state.fishes };
 
@@ -22,15 +24,25 @@ class App extends React.Component {
     });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh seafood market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
-
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,11 +2,17 @@ import React from "react";
 import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component {
+  loadSampleFishes = () => {
+    alert("samples are loading");
+    console.log("samples");
+  };
+
   render() {
     return (
       <div className="inventory">
         <h2>Inventory </h2>
         <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSampleFishes}>Load samples</button>
       </div>
     );
   }
